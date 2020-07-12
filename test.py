628709d665d1ef74d854cae75082c014c8be65dd
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-07-12 16:52:12 +0200

    ajout de test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+from classe_mysql import MysqlConnector
+from classeBDD import DatabaseManager, CategoryManager
+
+connector = MysqlConnector("localhost", "root", "cP93*mR78.")
+connector.use_db()
+
+pizza = CategoryManager(connector, "Pizzas")
+pizza.insert()
+pizza.select()
