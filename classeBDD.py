604fd6d0543c58327bce66d74e11330f0f9b435b
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-07-13 17:28:52 +0200

    test classe product et category : OK, y a plus qu'a faire la classe API

--- classeBDD.py
+++ classeBDD.py
@@ -51,9 +51,9 @@ class CategoryManager:
         self.connector = connector
         self.name_category = category
 
-    def delete(self):
-        req = "DELETE FROM Categories WHERE name_cat = %s"
-        self.connector.execute_info(req, (self.name_category,))
+    def delete(self, id_delete):
+        req = "DELETE FROM Categories WHERE id = %s"
+        self.connector.execute_info(req, (id_delete,))
 
     def insert(self):
         req = "INSERT INTO Categories (name_cat) VALUES (%s)"
@@ -66,7 +66,9 @@ class CategoryManager:
 
     def select(self):
         req = "SELECT * FROM Categories"
-        self.connector.select(req)
+        select = self.connector.select(req)
+        for x in select:
+            print("id {} : {}".format(x[0], x[1]))
 
     def get(self, id_cat):
         req = "SELECT name_cat FROM Categories WHERE id = %s"
@@ -75,55 +77,74 @@ class CategoryManager:
 
 class ProductManager:
 
-    def __init__(self):
-        self.id = int
-        self.name_product = str
-        self.brand = str
-        self.category_id = int
-        self.nutri_score = int
-        self.store = str
-        self.ingredient = str
+    def __init__(self, connector, name_product, brand, category_id, nutri_score, store, ingredient):
 
-    def delete(self, mysql_cmd, conn, cursor, id_product_delete):
+        self.connector = connector
+        self.name_product = name_product
+        self.brand = brand
+        self.category_id = category_id
+        self.nutri_score = nutri_score
+        self.store = store
+        self.ingredient = ingredient
+
+    def delete(self, id_delete):
         req = "DELETE FROM Product WHERE id = %s"
-        mysql_cmd.exec_infos(cursor, req, (id_product_delete,))
-        mysql_cmd.commit(conn)
-        mysql_cmd.cursor_close(cursor)
+        self.connector.execute_info(req, (id_delete,))
 
-    def insert(self, mysql_cmd, conn, cursor, product):
-        req = "INSERT INTO Product (name_product, brand, category_id, nutri_score, store, ingredient) " \
+    def insert(self):
+        req = "INSERT INTO Product " \
+              "(name_product, brand, category_id, nutri_score, store, ingredient) " \
               "VALUES (%s, %s, %s, %s, %s, %s)"
-        mysql_cmd.exec_infos(cursor, req, product)
-        mysql_cmd.commit(conn)
-        mysql_cmd.cursor_close(cursor)
+        self.connector.execute_info(req, (self.name_product,
+                                          self.brand,
+                                          self.category_id,
+                                          self.nutri_score,
+                                          self.store,
+                                          self.ingredient))
 
-    def update(self, mysql_cmd, conn, cursor, id_product_update, product):
-        infos = (product[0], product[1], product[2], product[3], product[4],product[5], id_product_update)
-        req = "UPDATE Categories " \
-              "SET name_product= %s, brand= %s, category_id= %s, nutri_score= %s, store= %s, ingredient= %s " \
-              "WHERE id = %s"
-        mysql_cmd.exec_infos(cursor, req, infos)
-        mysql_cmd.commit(conn)
-        mysql_cmd.cursor_close(cursor)
+    def update(self, id_product, item_update, value_item):
+        req = ""
+        if item_update == "name_product":
+            req = "UPDATE Categories SET name_product= %s " \
+                  "WHERE id = %s"
+        elif item_update == "brand":
+            req = "UPDATE Categories SET brand= %s " \
+                  "WHERE id = %s"
+        elif item_update == "categoy_id":
+            req = "UPDATE Categories SET category_id= %s " \
+                  "WHERE id = %s"
+        elif item_update == "nutri_score":
+            req = "UPDATE Categories SET nutri_score= %s " \
+                  "WHERE id = %s"
+        elif item_update == "store":
+            req = "UPDATE Categories SET store= %s " \
+                  "WHERE id = %s"
+        elif item_update == "ingredient":
+            req = "UPDATE Categories SET ingredient= %s " \
+                  "WHERE id = %s"
+        self.connector.execute_info(req, (value_item, id_product))
 
-    def select(self, mysql_cmd, cursor):
+    def select(self):
         req = "SELECT * FROM Product"
-        mysql_cmd.exec(cursor, req)
-        product_list = mysql_cmd.fetchall(cursor)
-        for product in product_list:
-            print(" product {} : {}".format(product[0], product[1]))
-        mysql_cmd.cursor_close(cursor)
+        select = self.connector.select(req)
+        for product in select:
+            print("id : {} "
+                  "name : {} "
+                  "brand : {} "
+                  "category_id : {} "
+                  "nutriscore : {} "
+                  "store : {} "
+                  "ingredients : {}".format(product[0], product[1], product[2], product[3], product[4], product[5], product[6]))
 
-    def get(self, mysql_cmd, cursor, id_product):
+    def get(self, id_product):
         req = "SELECT name_product, brand, Categories.name_cat, nutri_score, store, ingredient FROM Product " \
               "INNER JOIN Categories ON Categories.id = Product.category_id " \
-              "WHERE id = %s"
-        mysql_cmd.exec_infos(cursor, req, (id_product,))
-        product = mysql_cmd.fetchall(cursor)
-        print("name : {} "
-              "brand : {}"
-              "category : {}"
-              "nutriscore : {}"
-              "store : {}"
-              "ingredients : {}".format(product[0], product[1], product[2], product[3], product[4], product[5]))
-        mysql_cmd.cursor_close(cursor)
+              "WHERE Product.id = %s"
+        select = self.connector.select_info(req, (id_product,))
+        for product in select:
+            print("name : {} \n"
+                  "brand : {} \n"
+                  "category : {} \n"
+                  "nutriscore : {} \n"
+                  "store : {} \n"
+                  "ingredients : {}".format(product[0], product[1], product[2], product[3], product[4], product[5]))
