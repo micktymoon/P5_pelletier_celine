96430c1cfd373fa0702139ecb57cb41d14fd0cd8
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-07-22 20:16:03 +0200

    ajout des docstrings

--- classe_api.py
+++ classe_api.py
@@ -5,16 +5,42 @@ import json
 import requests
 
 
-class ApiSearch:
+class ApiManageSearch:
+    """
+    Class that manages the search in the OpenFoodFact API.
+
+    Methods:
+        search_product()
+            Search a product in OpenFoodFact API and return the first three
+             products found.
+
+        search_cat()
+            Search if in the OpenFoodFact API, the category given in parameter
+             exists. And return this category.
+    """
 
     def __init__(self):
-        self.api_search_terms = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1&search_terms="
+        """ ApiManagerSearch class constructor."""
+        self.api_search_terms = "https://fr.openfoodfacts.org/cgi/search.pl?" \
+                                "action=process&json=1&search_terms="
         self.api_search_cat = "https://fr.openfoodfacts.org/categories.json"
 
     def search_product(self, name_product):
         """
-        Search a product in Open Food Fact API and return the 1st product found
+        Search a product in OpenFoodFact API and return the first
+         three products found.
 
+        Complete the link with the name of the product you are looking for,
+         search for the corresponding products and return the first
+         three products found.
+
+        Parameters:
+            name_product: str
+                The name of the product we're looking for.
+
+        Returns:
+            list
+                Alist of the first three product found.
         """
         final_http = self.api_search_terms + name_product
         response = requests.get(final_http)
@@ -26,14 +52,32 @@ class ApiSearch:
             final_product = {"name": path["product_name"],
                              "brand": path.get("brands", None),
                              "category": path.get("categories", None),
-                             "nutri_score": path.get("nutriscore_grade", None),
+                             "nutri_score": path.get(
+                                 "nutriscore_grade", None),
                              "store": path.get("stores", None),
-                             "ingredients": path.get("ingredients_text", None)}
+                             "ingredients": path.get(
+                                 "ingredients_text", None)}
             list_product.append(final_product)
             i += 1
         return list_product
 
     def search_cat(self, name_cat):
+        """
+        Search if in the OpenFoodFact API, the category given in parameter
+         exists. And return this category.
+
+        Search for all the categories that exist in the OpenFoodFact API, and
+         sees if the category passed as a parameter exists in its categories.
+         If so, return the category.
+
+        Parameters:
+            name_cat: str
+                The name of the category we're looking for.
+
+        Returns:
+            str
+                The name of the category we're looking for.
+             """
         response = requests.get(self.api_search_cat)
         response_text = json.loads(response.text)
         listcat = []
@@ -42,8 +86,3 @@ class ApiSearch:
         for cat in listcat:
             if name_cat in listcat[cat]:
                 return name_cat
-
-
-
-
-
