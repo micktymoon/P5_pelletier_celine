c5a50db3b81f2876e29c04c9fb52165eb9e1a39c
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-08-09 15:54:30 +0200

    doc + flake8 ok

--- function_updating_db.py
+++ function_updating_db.py
@@ -28,13 +28,15 @@ def get_store_and_add_to_db(sm, text):
 
 
 def associate_cat_to_product(cm, pcm, product):
-    """Associate a category to a product in the database and the category
-        to Categories table if it doesn't exist.
+    """
+    Associate a category to a product in the database and the category
+     to Categories table if it doesn't exist.
 
     Get a list of product categories.
     Checks if the product categories corresponds to one of the
      categories of the database, and if yes, associates the product with
-     the category. But if the category doesn't exist in the Categories table
+     the category.
+    But if the category doesn't exist in the Categories table
      of the database, insert the category in the table and then associate it
      with the product.
 
@@ -57,24 +59,25 @@ def associate_cat_to_product(cm, pcm, product):
 
 
 def associate_store_to_product(sm, psm, product):
-    """Associate a store to a product in the database and the store to Store
-        table if it doesn't exist.
+    """
+    Associate a store to a product in the database and the store to Store
+     table if it doesn't exist.
 
-        Get a list of product store.
-        Checks if the product stores corresponds to one of the
-         stores of the database, and if yes, associates the product with
-         the store. But if the store doesn't exist in the Store table of
-         the database, insert the store in the table and then associate it
-         with the product.
+    Get a list of product store.
+    Checks if the product stores corresponds to one of the
+     stores of the database, and if yes, associates the product with
+     the store. But if the store doesn't exist in the Store table of
+     the database, insert the store in the table and then associate it
+     with the product.
 
-        Parameters:
-            sm : class StoreManager
-                The manager of the Store table in the database.
-            psm : class ProductStoreManager
-                The manager of the ProductStore table in the database.
-            product : dict
-                The product to whiwh we want to associate a store.
-         """
+    Parameters:
+        sm : class StoreManager
+            The manager of the Store table in the database.
+        psm : class ProductStoreManager
+            The manager of the ProductStore table in the database.
+        product : dict
+            The product to whiwh we want to associate a store.
+    """
     for sto in product["store"]:
         check = sm.select(name=sto)
         if check is None:
@@ -86,10 +89,42 @@ def associate_store_to_product(sm, psm, product):
 
 
 def insert_product_db(pm, cm, sm, pcm, psm, prod):
+    """
+    Insert a given product in the database.
+
+    Retrieve the product stores and insert them into the database.
+    Insert the product in the database.
+    Associates the product with its stores.
+    Retrieves the product categories and associates them with it.
+    And return the product.
+
+    Parameters:
+        pm : class ProductManager
+            The manager of the Product table in the database.
+
+        cm : class CategoryManager
+            The manager of the Categories table in the database.
+
+        sm : class StoreManager
+            The Manager of the Categories table in the database.
+
+        pcm : class ProductCategoryManager
+            The manager of the ProductCategory table in the database.
+
+        psm : class ProductStoreManager
+            The manager of the ProductStore table in the database.
+
+        prod : dict
+            The product that we want to insert into the database.
+
+    Returns:
+        dict
+            The product that has been inserted into the database.
+    """
     get_store_and_add_to_db(sm, prod["store"])
     product = pm.insert(pcm, psm, product=prod)
     associate_store_to_product(sm, psm, product)
-    product["store"] = psm.select_association_with_id_prod(product["id"])
+    product["store"] = psm.select_asso_with_id_prod(product["id"])
     associate_cat_to_product(cm, pcm, product)
-    product["category"] = pcm.select_association_with_id_prod(product["id"])
+    product["category"] = pcm.select_asso_with_id_prod(product["id"])
     return product
